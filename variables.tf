f96ab9bd821c9d38a57ced661db86a3a47ebfdb7
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-09-21 16:39:46 +0530

    updated infra

--- variables.tf
+++ variables.tf
@@ -1,9 +1,6 @@
 variable "project_name" {
-
-    default = "roboshop"
-  
+  default = "roboshop"
 }
-
 
 variable "cidr_block" {
   default = "10.0.0.0/16"
@@ -11,16 +8,11 @@ variable "cidr_block" {
 
 variable "common_tags" {
   default = {
-
     Project = "roboshop"
-
-    Environment= "Dev"
-
+    Environment = "DEV"
     Terraform = "true"
-
   }
 }
-
 
 variable "public_subnet_cidr" {
   default = ["10.0.1.0/24","10.0.2.0/24"]
@@ -33,3 +25,35 @@ variable "private_subnet_cidr" {
 variable "database_subnet_cidr" {
   default = ["10.0.21.0/24","10.0.22.0/24"]
 }
+
+variable "sg_ingress_rules" {
+  default = [
+    {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      description = "allowing all traffic from internet"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "instances" {
+  default = {
+    MongoDB = "t3.medium"
+    MySQL = "t3.medium"
+    Redis = "t2.micro"
+    RabbitMQ = "t2.micro"
+    Catalogue = "t2.micro"
+    User = "t2.micro"
+    Cart = "t2.micro"
+    Shipping = "t2.micro"
+    Payment = "t2.micro"
+    Web = "t2.micro"
+  }
+}
+
+variable "zone_name" {
+  default = "desharaju.online"
+}
+
